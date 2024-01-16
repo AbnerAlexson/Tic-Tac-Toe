ef269c8df884030e6009c6be3536ea1a381927b6
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2024-01-16 09:03:07 +0800

    initialize pseudocode into javascript using comments

---
+++ main.js
@@ -0,0 +1,15 @@
+// get user data from the game settings option
+
+// attach gametile to event listner.
+
+// and then start the game
+
+// program need to check what gamemode we're playing
+
+// we need to set win condition
+
+// we need to determine current player
+
+// after each players move check if win condition is meant.
+// HvH, HvEAI, HvHAI
+
