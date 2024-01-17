439ef49791dd7a8194ae60f482c8e251ae9e747a
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2024-01-17 14:00:00 +0800

    update main.js
    made a functionality that game setting from main page
    
    created a function that initialize game variable

--- main.js
+++ main.js
@@ -13,3 +13,41 @@
 // after each players move check if win condition is meant.
 // HvH, HvEAI, HvHAI
 
+const form = document.querySelector('#settingForm');
+
+// collects user data
+form.addEventListener('submit', (event) => {
+    event.preventDefault() // prevent submit button from refreshing the page
+
+    // function that extracts data and turns it to an object
+    function collectData () {
+        const player1Name = document.querySelector('#form1').value;
+        const player2Name = document.querySelector('#form2').value;
+        let choice = document.querySelector('input[name="choice"]:checked').value;
+        return { player1Name, player2Name, choice }
+    };
+
+    gameData = collectData(); //store data
+    document.querySelector('.modal-wrapper').setAttribute('hidden', true); // closes the modal after submiting data
+    //console.log(gameData) // for testing should be removed
+    startGame(gameData)
+});
+
+const initializeVariable = (varData) => {
+    // initializing game variable
+    varData.choice = +gameData.choice;
+    varData.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    varData.player1 = 'X';
+    varData.player2 = 'O';
+    varData.round = 0;
+    varData.currentPlayer = 'X';
+    varData.gameOver = false;
+}
+
+const startGame = (gameData) => {
+    // initialize game variables
+    // atached event listeners to gameboard
+    initializeVariable(gameData)
+    console.log(gameData)
+    
+}
