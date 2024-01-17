572162950429906c1266e348db6ac1b3c1ea5463
Abner Alexon <146349299+AbnerAlexson@users.noreply.github.com>
2024-01-17 15:18:21 +0800

    added endGameCondition

--- main.js
+++ main.js
@@ -13,6 +13,17 @@
 // after each players move check if win condition is meant.
 // HvH, HvEAI, HvHAI
 
+const endGameCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
 const form = document.querySelector('#settingForm');
 
 // collects user data
